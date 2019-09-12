8eb90266070e6c1f99a240da721252421144a2c5
Borovov Dmitriy <me@dmitriyq.ru>
2019-09-13 01:18:25 +0300

    fix

--- Dns.Resolver.Aggregator/Messages/DomainResolvedMessageHandler.cs
+++ Dns.Resolver.Aggregator/Messages/DomainResolvedMessageHandler.cs
@@ -12,16 +12,18 @@ namespace Dns.Resolver.Aggregator.Messages
 	public class DomainResolvedMessageHandler : IMessageQueueHandler<DomainResolvedMessage>
 	{
 		private readonly IDomainAggregatorService _aggregatorService;
+		private readonly ILogger<DomainResolvedMessageHandler> _logger;
 
-		public DomainResolvedMessageHandler(IDomainAggregatorService aggregatorService)
+		public DomainResolvedMessageHandler(IDomainAggregatorService aggregatorService, ILogger<DomainResolvedMessageHandler> logger)
 		{
 			_aggregatorService = aggregatorService;
+			_logger = logger;
 		}
 
 		public Task<bool> Handle(DomainResolvedMessage message)
 		{
 			if (message == null) throw new ArgumentNullException(nameof(message));
-
+			_logger.LogInformation($"Handled ID {message.TraceId} - {message.Name}");
 			_aggregatorService.AddDomain(message);
 			return Task.FromResult(true);
 		}
