e1751cf1fa3ef929a4cbf68d88f1d1c82e976a14
Borovov Dmitriy <me@dmitriyq.ru>
2019-09-25 00:43:11 +0300

    update hyperlocal config

--- Hyperlocal/start.sh
+++ Hyperlocal/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-service bind9 start
+chmod -R 0777 /var/log
+chmod -R 0777 /var/named
+chmod -R 0777 /etc
+echo "" > /var/log/named.log
+
+chmod -R 0777 /var/log
+
+/usr/bin/named -f -u named -4 &
 
 echo nameserver 127.0.0.1 > /etc/resolv.conf
 
-tail -f /var/lib/bind/bind.log
+tail -f /var/log/named.log
 
 exec "$@"
-
