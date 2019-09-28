0adf66a1469a18b3ec6d59ffa53467ffb770fcfc
Borovov Dmitriy <me@dmitriyq.ru>
2019-09-28 15:19:36 +0300

    little refactoring code

--- Dns.Resolver.Consumer/Services/DomainLookupService.cs
+++ Dns.Resolver.Consumer/Services/DomainLookupService.cs
@@ -37,28 +37,17 @@ namespace Dns.Resolver.Consumer.Services
 				return (ips: ips, code: ResponseCode.NoError);
 			}
 			catch (ResponseException re) when (re.Response.ResponseCode == ResponseCode.NameError
-				|| re.Response.ResponseCode == ResponseCode.ServerFailure 
+				|| re.Response.ResponseCode == ResponseCode.ServerFailure
 				|| re.Message == "No matching records")
 			{
 				_semaphoreSlim.Release();
 				return (ips: new HashSet<string>(), code: re.Response.ResponseCode);
 			}
-			catch (OperationCanceledException oce)
+			catch (OperationCanceledException)
 			{
 				_logger.LogWarning($"OperationCanceledException for {domain}");
-				try
-				{
-					var resp = await _dnsClient.Lookup(_idnMapping.GetAscii(domain)).ConfigureAwait(false);
-					var ips = resp.Select(x => x.ToString()).ToHashSet();
-					_semaphoreSlim.Release();
-					return (ips: ips, code: ResponseCode.NoError);
-				}
-				catch (Exception ex1)
-				{
-					_logger.LogWarning(ex1, $"After second retry: {ex1.Message}");
-					_semaphoreSlim.Release();
-					throw;
-				}
+				_semaphoreSlim.Release();
+				throw;
 			}
 			catch (Exception ex)
 			{
